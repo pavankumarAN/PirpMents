19f44efef57884012f81e914bc232aae2d30c315
PavankumarAN <pavankumaran51@gmail.com>
2019-04-04 23:36:32 +0530

    Assignment 1

---
+++ FrontEnd-Fundamentals/JS(ES6)/Assignment1/song.js
@@ -0,0 +1,44 @@
+//string varibles
+var songName = "Matte male aagide";
+var singerName = "Armaan Malik";
+var musicDirectorName = "Arjun Janya";
+var movieName = "Chakravarthi";
+var janre = "romantic";
+
+//Numbers
+var awards = 0;
+var rating = 3.2;
+
+//Boolean
+var hit = false;
+
+//Other data type
+var profit = null;
+
+//Array
+var cast = ["Darshan", "Deepa", "Srujan"];
+
+//Object
+var details = {
+    language : "Kannada",
+    numberOfSongs : 4,
+    fights : 5
+};
+
+/*
+    Here I console them into the developer tool
+    I hope it is clear.
+*/
+
+console.log(songName);
+console.log(singerName);
+console.log(musicDirectorName);
+console.log(movieName);
+console.log(janre);
+console.log(awards);
+console.log(rating);
+console.log(hit);
+console.log(profit);
+console.log(cast);
+console.log(details);
+
