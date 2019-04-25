8f600795626b67c23af99e1116f687118daceac8
PavankumarAN <pavankumaran51@gmail.com>
2019-04-25 06:55:27 +0530

    DOM

--- FrontEnd-Fundamentals/JS(ES6)/DOM/dom.js
+++ FrontEnd-Fundamentals/JS(ES6)/DOM/dom.js
@@ -1,6 +1,13 @@
-var pTags = document.getElementsByTagName('p');
-console.log(pTags);
+// var pTags = document.getElementsByTagName('p');
+// console.log(pTags);
 
-for(let i=0; i<pTags.length; i++) {
-    pTags[i].innerHTML = "<h1>Pavan</h1>"
-}
+// for(let i=0; i<pTags.length; i++) {
+//     pTags[i].innerHTML = "<h1>Pavan</h1>"
+// }
+
+
+const divs = document.querySelectorAll(".divi");
+console.log(divs);
+divs.forEach(function(div){
+  div.style.color = "red";
+});
